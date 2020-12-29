c17bf1a10f42c13d2c5d61c607adf6d14da2571b
a-nikolskii <alexandr.nikolskii@gmail.com>
2020-12-29 18:07:37 +0300

    lesson6

--- QtLanguage_en.ts
+++ QtLanguage_en.ts
@@ -4,73 +4,95 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.cpp" line="36"/>
+        <location filename="mainwindow.cpp" line="33"/>
+        <location filename="mainwindow.cpp" line="295"/>
         <source>&amp;File</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="37"/>
+        <location filename="mainwindow.cpp" line="44"/>
+        <location filename="mainwindow.cpp" line="303"/>
         <source>&amp;Tools</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="38"/>
+        <location filename="mainwindow.cpp" line="48"/>
+        <location filename="mainwindow.cpp" line="306"/>
         <source>&amp;Help</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="41"/>
+        <location filename="mainwindow.cpp" line="60"/>
+        <location filename="mainwindow.cpp" line="296"/>
         <source>New File</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="46"/>
+        <location filename="mainwindow.cpp" line="64"/>
+        <location filename="mainwindow.cpp" line="297"/>
         <source>Open File...</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="51"/>
-        <source>Open File in RO mode...</source>
+        <location filename="mainwindow.cpp" line="68"/>
+        <location filename="mainwindow.cpp" line="298"/>
+        <source>Open for read...</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="55"/>
+        <location filename="mainwindow.cpp" line="72"/>
+        <location filename="mainwindow.cpp" line="299"/>
         <source>Save</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="60"/>
-        <source>Save As...</source>
+        <location filename="mainwindow.cpp" line="76"/>
+        <location filename="mainwindow.cpp" line="300"/>
+        <source>Save as ...</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="64"/>
-        <source>&amp;Quit</source>
+        <location filename="mainwindow.cpp" line="79"/>
+        <location filename="mainwindow.cpp" line="223"/>
+        <source>Print</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="80"/>
+        <location filename="mainwindow.cpp" line="83"/>
+        <location filename="mainwindow.cpp" line="301"/>
+        <source>Quit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="92"/>
+        <location filename="mainwindow.cpp" line="304"/>
         <source>&amp;Language</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="140"/>
+        <location filename="mainwindow.cpp" line="154"/>
         <source>Save File</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="142"/>
+        <location filename="mainwindow.cpp" line="156"/>
         <source>all(*.*);;*.txt(*.txt)</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="157"/>
+        <location filename="mainwindow.cpp" line="171"/>
         <source>Open File</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="159"/>
+        <location filename="mainwindow.cpp" line="173"/>
         <source>*.txt(*.txt);;all(*.*)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="201"/>
+        <location filename="mainwindow.cpp" line="309"/>
+        <source>New File.txt</source>
         <translation></translation>
     </message>
 </context>
--- QtLanguage_ru.ts
+++ QtLanguage_ru.ts
@@ -4,84 +4,96 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.cpp" line="32"/>
+        <location filename="mainwindow.cpp" line="33"/>
+        <location filename="mainwindow.cpp" line="295"/>
         <source>&amp;File</source>
         <translation>Файл</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="33"/>
+        <location filename="mainwindow.cpp" line="44"/>
+        <location filename="mainwindow.cpp" line="303"/>
         <source>&amp;Tools</source>
-        <translation>Инструменты</translation>
+        <translation type="unfinished">Инструменты</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="34"/>
-        <source>&amp;Language</source>
-        <translation>Язык</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.cpp" line="35"/>
+        <location filename="mainwindow.cpp" line="48"/>
+        <location filename="mainwindow.cpp" line="306"/>
         <source>&amp;Help</source>
         <translation>Справка</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="38"/>
+        <location filename="mainwindow.cpp" line="60"/>
+        <location filename="mainwindow.cpp" line="296"/>
         <source>New File</source>
         <translation>Новый файл</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="43"/>
+        <location filename="mainwindow.cpp" line="64"/>
+        <location filename="mainwindow.cpp" line="297"/>
         <source>Open File...</source>
-        <translation>Открыть файл...</translation>
+        <translation>Открыть файл</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="48"/>
-        <source>Open File in RO mode...</source>
-        <translation>Открыть файл для чтения</translation>
+        <location filename="mainwindow.cpp" line="68"/>
+        <location filename="mainwindow.cpp" line="298"/>
+        <source>Open for read...</source>
+        <translation>Открыть для чтения</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="52"/>
+        <location filename="mainwindow.cpp" line="72"/>
+        <location filename="mainwindow.cpp" line="299"/>
         <source>Save</source>
         <translation>Сохранить</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="57"/>
-        <source>Save As...</source>
+        <location filename="mainwindow.cpp" line="76"/>
+        <location filename="mainwindow.cpp" line="300"/>
+        <source>Save as ...</source>
         <translation>Сохранить как...</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="61"/>
-        <source>&amp;Quit</source>
+        <location filename="mainwindow.cpp" line="79"/>
+        <location filename="mainwindow.cpp" line="223"/>
+        <source>Print</source>
+        <translation>Печать</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="83"/>
+        <location filename="mainwindow.cpp" line="301"/>
+        <source>Quit</source>
         <translation>Выход</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="72"/>
-        <source>English</source>
-        <translation>Английский</translation>
+        <location filename="mainwindow.cpp" line="92"/>
+        <location filename="mainwindow.cpp" line="304"/>
+        <source>&amp;Language</source>
+        <translation>Язык</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="73"/>
-        <source>Russian</source>
-        <translation>Русский</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.cpp" line="120"/>
+        <location filename="mainwindow.cpp" line="154"/>
         <source>Save File</source>
-        <translation>Сохрание файла</translation>
+        <translation>Сохранение файла</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="122"/>
+        <location filename="mainwindow.cpp" line="156"/>
         <source>all(*.*);;*.txt(*.txt)</source>
-        <translation>все(*.*);;*.txt.(*.txt)</translation>
+        <translation>все(*.*);;*.txt(*.txt)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="137"/>
+        <location filename="mainwindow.cpp" line="171"/>
         <source>Open File</source>
         <translation>Открытие файла</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="139"/>
+        <location filename="mainwindow.cpp" line="173"/>
         <source>*.txt(*.txt);;all(*.*)</source>
-        <translation>все(*.*);;*.txt.(*.txt)</translation>
+        <translation>*.txt(*.txt);;все(*.*)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="201"/>
+        <location filename="mainwindow.cpp" line="309"/>
+        <source>New File.txt</source>
+        <translation>Новый файл.txt</translation>
     </message>
 </context>
 </TS>
